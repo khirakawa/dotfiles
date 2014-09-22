cd741086e46d2f0d6a1165f2b4c4cc99ebec7de7
khirakawa <ken.hirakawa@am.sony.com>
2014-09-22 13:21:37 -0700

    Updated zshrc.symlink and changed $ZSH to $DOTFILES

--- system/path.zsh
+++ system/path.zsh
@@ -3,6 +3,6 @@ export JAVA_HOME=$(/usr/libexec/java_home)
 export TOMCAT_HOME="/Users/khirakawa/apache-tomcat-7.0.52"
 export DART_BIN="/Users/khirakawa/dart/dart-sdk/bin/"
 export GO_BIN="/usr/local/go/bin"
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$ZSH/cisco:/usr/local/share/npm/bin:$KDIFF3:$DART_BIN:$GO_BIN:$PATH"
+export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$DOTFILES/bin:$DOTFILES/cisco:/usr/local/share/npm/bin:$KDIFF3:$DART_BIN:$GO_BIN:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
