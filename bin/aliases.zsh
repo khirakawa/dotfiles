30eed936b3054241613292b0de81e8ca2a5c0d3c
khirakawa <ken.hirakawa@am.sony.com>
2014-09-22 00:45:34 -0700

    Updated .dotfiles

--- bin/aliases.zsh
+++ bin/aliases.zsh
@@ -1,2 +1,2 @@
 alias dev="cd ~/work/swordfish";
-alias swordfish="cd ~/work/swordfish && static --cache 1"
+alias swordfish="cd ~/work/swordfish && static --cache 1 --port 8008"
--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,8 @@
-export JAVA_HOME="/Library/Java/Home"
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$ZSH/cisco:/usr/local/share/npm/bin:$PATH"
+export KDIFF3="/Applications/kdiff3.app/Contents/MacOS"
+export JAVA_HOME=$(/usr/libexec/java_home)
+export TOMCAT_HOME="/Users/khirakawa/apache-tomcat-7.0.52"
+export DART_BIN="/Users/khirakawa/dart/dart-sdk/bin/"
+export GO_BIN="/usr/local/go/bin"
+export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$ZSH/cisco:/usr/local/share/npm/bin:$KDIFF3:$DART_BIN:$GO_BIN:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
