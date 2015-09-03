e366556c5e565da05e1178ae81ee6af80be10e4a
khirakawa <ken.hirakawa@am.sony.com>
2015-09-03 10:54:10 +0900

    Update bin

--- bin/aliases.zsh
+++ bin/aliases.zsh
@@ -1,2 +1,4 @@
 alias dev="cd ~/work/swordfish";
-alias swordfish="cd ~/work/swordfish && static --cache 1 --port 8008"
+alias swordfish="cd ~/work/swordfish && static --cache 1 --port 8008 -a 0.0.0.0"
+alias go='nocorrect go'
+alias burp='rm -rf node_modules bower_components dist tmp; npm cache clean && bower cache clean; npm install && bower install'
--- system/path.zsh
+++ system/path.zsh
@@ -1,8 +1,10 @@
 export KDIFF3="/Applications/kdiff3.app/Contents/MacOS"
 export JAVA_HOME=$(/usr/libexec/java_home)
 export TOMCAT_HOME="/Users/khirakawa/apache-tomcat-7.0.52"
-export DART_BIN="/Users/khirakawa/dart/dart-sdk/bin/"
-export GO_BIN="/usr/local/go/bin"
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$DOTFILES/bin:$DOTFILES/cisco:/usr/local/share/npm/bin:$KDIFF3:$DART_BIN:$GO_BIN:$PATH"
+export DART_BIN="/Users/khirakawa/dart/dart-sdk/bin"
+export PROJECT_HOME="/Users/khirakawa/projects"
+export GOPATH="$PROJECT_HOME/golang"
+export GOBIN="$GOPATH/bin"
+export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$DOTFILES/bin:$DOTFILES/cisco:/usr/local/share/npm/bin:$KDIFF3:$DART_BIN:$GOBIN:$GOPATH:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
